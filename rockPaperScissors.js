492db9cdb52e78a4dbeaba8cb38927312a3f946f
Roberto Paz <fsjedi@outlook.com>
2021-09-18 14:01:33 -0400

    terminan condicionales usuario

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -2,3 +2,9 @@ const getUserChoice = userInput => {
     userInput = userInput.toLowerCase();
     if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
       return userInput;
+    } else {
+        console.log('error');
+      }
+    };
+    
+    //console.log(getUserChoice('ros'))
