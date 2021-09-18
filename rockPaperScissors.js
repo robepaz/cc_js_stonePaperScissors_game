8401cb492938addeba5cd2bb6b4d9e5d37d19545
Roberto Paz <fsjedi@outlook.com>
2021-09-18 14:08:28 -0400

    switch corregido

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,23 +1,24 @@
-const getUserChoice = userInput => {
-    userInput = userInput.toLowerCase();
-    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
-      return userInput;
-    } else {
-        console.log('error');
-      }
-    };
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase()
+  if (
+    userInput === 'rock' ||
+    userInput === 'paper' ||
+    userInput === 'scissors'
+  ) {
+    return userInput
+  } else {
+    console.log('error')
+  }
+}
 
 const getComputerChoice = () => {
-    const randomNumber = Math.floor(Math.random() * 3) 
-    switch(randomNumber) {
-        case 0:
-            return 'rock';
-            break;
-          case 1:
-            return 'paper';
-            break;
-          case 2: 
-            return 'scissors';
-            break;
-          }
-        }
+  const randomNumber = Math.floor(Math.random() * 3)
+  switch (randomNumber) {
+    case 0:
+      return 'rock'
+    case 1:
+      return 'paper'
+    case 2:
+      return 'scissors'
+  }
+}
