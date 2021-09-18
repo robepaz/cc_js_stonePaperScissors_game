ee29d79709729074cb1c9b06210b6599ebe9e8ef
Roberto Paz <fsjedi@outlook.com>
2021-09-18 14:07:06 -0400

    comienza switch

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -10,3 +10,14 @@ const getUserChoice = userInput => {
 const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3) 
     switch(randomNumber) {
+        case 0:
+            return 'rock';
+            break;
+          case 1:
+            return 'paper';
+            break;
+          case 2: 
+            return 'scissors';
+            break;
+          }
+        }
